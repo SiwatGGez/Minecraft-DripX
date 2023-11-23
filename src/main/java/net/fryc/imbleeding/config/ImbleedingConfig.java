a690888885a6dace7223c8cb267c1767b76e58a0
Xires87 <frycmod@gmail.com>
2023-11-23 17:22:51 +0100

    and now increased by 10

--- src/main/java/net/fryc/imbleeding/config/ImbleedingConfig.java
+++ src/main/java/net/fryc/imbleeding/config/ImbleedingConfig.java
@@ -49,7 +49,7 @@ public class ImbleedingConfig implements ConfigData {
 
     @ConfigEntry.Category("bleeding_length")
     @ConfigEntry.Gui.Tooltip
-    public int arrowBleedLength = 90;
+    public int arrowBleedLength = 100;
 
     @ConfigEntry.Category("bleeding_length")
     @ConfigEntry.Gui.Tooltip
