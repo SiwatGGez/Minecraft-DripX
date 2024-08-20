fa1abb7b239eb87257b24b9bc6281756723730f5
Xires87 <frycmod@gmail.com>
2024-08-20 12:12:16 +0200

    removed minecraft's randoms to avoid crashes

--- src/main/java/net/fryc/imbleeding/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/fryc/imbleeding/mixin/ServerPlayerEntityMixin.java
@@ -17,9 +17,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Random;
+
 @Mixin(ServerPlayerEntity.class)
 abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
+    private Random myRandom = new Random();
 
     public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
@@ -29,12 +32,12 @@ abstract class ServerPlayerEntityMixin extends PlayerEntity {
     private void spawnBloodParticles(CallbackInfo info) {
         ServerPlayerEntity dys = ((ServerPlayerEntity)(Object)this);
         if(dys.hasStatusEffect(ModEffects.BLEED_EFFECT)){
-            if(dys.getRandom().nextInt(100) < 5 + dys.getActiveStatusEffects().get(ModEffects.BLEED_EFFECT).getAmplifier()*5){
+            if(this.myRandom.nextInt(100) < 5 + dys.getActiveStatusEffects().get(ModEffects.BLEED_EFFECT).getAmplifier()*5){
                 Vec3d vec3d = dys.getVelocity();
                 PacketByteBuf buf = PacketByteBufs.create();
-                buf.writeDouble(dys.getX() + (dys.getRandom().nextFloat()/2) - 0.25f);
-                buf.writeDouble(dys.getY() + dys.getRandom().nextFloat() + 0.1f);
-                buf.writeDouble(dys.getZ() + (dys.getRandom().nextFloat()/2) - 0.25f);
+                buf.writeDouble(dys.getX() + (this.myRandom.nextFloat()/2) - 0.25f);
+                buf.writeDouble(dys.getY() + this.myRandom.nextFloat() + 0.1f);
+                buf.writeDouble(dys.getZ() + (this.myRandom.nextFloat()/2) - 0.25f);
                 buf.writeDouble(vec3d.getY()-0.05);
                 for (ServerPlayerEntity pl : PlayerLookup.tracking(dys.getServerWorld(), dys.getChunkPos())) {
                     ServerPlayNetworking.send(pl, ModPackets.CREATE_BLOOD_PARTICLE, buf);
